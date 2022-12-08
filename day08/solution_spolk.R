59e77c44ca1541b9dc75b06e0dd20dfbcd2ea6a4
Polk <sarpolk@umich.edu>
2022-12-08 12:43:55 +0100

    add day08 part 2

--- day08/solution_spolk.R
+++ day08/solution_spolk.R
@@ -47,3 +47,60 @@ i <- 1; iTree <- 1; invisible(apply(t(dat), 1, getRows))
 sum(trees != 0)
 
 # --- Part Two ---
+
+getRowsScore <- function(curRow) {
+  #run getTree function across rows
+  sapply(curRow, getTreeScore, curRow = curRow)
+  
+  #update indices
+  i <<- i + 1
+  iTree <<- 1
+  
+  #get output
+  return(trees)
+}
+
+getTreeScore <- function(curTree, curRow) {
+  #find which trees are blocking my view
+  tallerTrees <- curTree <= curRow 
+  
+  #find trees to the right and left of the current tree
+  treesR <- if (iTree != 1) which(1:nrow(dat) < iTree) else 0
+  treesL <- if (iTree != nrow(dat)) which(1:nrow(dat) > iTree) else 0
+  
+  #find blocking tree to the right and left, distance to current tree, update score
+  if (sum(treesR) != 0) {
+    anyTallerTrees <- treesR[which(tallerTrees[treesR] == T)]
+    numTrees <- ifelse(length(anyTallerTrees) > 0, abs(iTree - rev(anyTallerTrees)[1]), length(tallerTrees[treesR]))
+    trees[i, iTree] <<- trees[i, iTree] * numTrees
+  } else {
+    #edge trees = 0
+    trees[i, iTree] <<- 0
+  }
+  if (sum(treesL) != 0) {
+    anyTallerTrees <- treesL[which(tallerTrees[treesL] == T)]
+    numTrees <- ifelse(length(anyTallerTrees) > 0, abs(iTree - anyTallerTrees[1]), length(tallerTrees[treesL]))
+    trees[i, iTree] <<- trees[i, iTree] * numTrees
+  } else {
+    #edge trees = 0
+    trees[i, iTree] <<- 0
+  }
+  
+  #update tree index
+  iTree <<- iTree + 1
+}
+
+#create tree matrix, scores = 1
+trees <- matrix(rep(1, nrow(dat)*ncol(dat)), nrow = nrow(dat))
+
+#initialize indices and get rows across rows
+i <- 1; iTree <- 1; invisible(apply(dat, 1, getRowsScore))
+
+#transpose tree matrix, reinitialize indices, get rows across columns
+trees <- t(trees)
+i <- 1; iTree <- 1; invisible(apply(t(dat), 1, getRowsScore))
+
+#get answer
+max(trees)
+
+
